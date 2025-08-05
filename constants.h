167b2e81a208d0dc271dad00c06a3b4ff67a5e28
NotThinkAnything <nguyentuananh12042008@gmail.com>
2025-08-05 16:06:22 +0700

    adding some stuff

---
+++ constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+	constexpr int EMPTY = 9;
+	constexpr int PLAYER_0 = 0;
+	constexpr int PLAYER_1 = 1;
+	constexpr int MAX_GRIP = 3;
+	constexpr int MAX_TURN = 9;
+	constexpr int FIRST = 0;
+	constexpr int LAST = 2;
+	
+#endif
